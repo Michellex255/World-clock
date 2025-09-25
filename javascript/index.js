96404f9b983c765c60714674a08ab6eccdbd750d
Michelle958 <michelle4.dup@gmail.com>
2025-09-25 06:31:50 +0200

    Added the current location

--- javascript/index.js
+++ javascript/index.js
@@ -32,7 +32,9 @@ function updateTime() {
 
 function updateCity(event) {
   let cityTimeZone = event.target.value;
-
+  if (cityTimeZone === "current") {
+    cityTimeZone = moment.tz.guess();
+  }
   let cityTime = moment().tz(cityTimeZone);
   let citiesElement = document.querySelector("#cities");
   citiesElement.innerHTML = `
