d6c828fa301d9992e182d08e200ac7e94f92dac2
Michelle958 <michelle4.dup@gmail.com>
2025-09-25 06:25:43 +0200

    Improve JS timezone  & HTML display UX

--- javascript/index.js
+++ javascript/index.js
@@ -19,5 +19,33 @@ function updateTime() {
   londonTimeElement.innerHTML = londontime.format(
     "h:mm:ss [<small>]A[</small>]"
   );
+
+  let newYorkElement = document.querySelector("#new-york");
+  let newYorkDateElement = newYorkElement.querySelector(".date");
+  let newYorkTimeElement = newYorkElement.querySelector(".time");
+  let newYorktime = moment().tz("America/New_York");
+  newYorkDateElement.innerHTML = newYorktime.format("MMMM Do YYYY");
+  newYorkTimeElement.innerHTML = newYorktime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+}
+
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `
+  <div class="city">
+  <div>
+  <h2>${cityTimeZone.replace("_", " ").split("/")[1]}</h2>
+  <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+    </div>
+    <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format(
+    "A"
+  )}</small></div></div>`;
 }
 updateTime();
+
+let citiesSelect = document.querySelector("#city");
+citiesSelect.addEventListener("change", updateCity);
